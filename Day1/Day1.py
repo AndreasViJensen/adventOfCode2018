100394356a1ab4e041d52b68a8d177fa3d61615c
andreasvijensen <andreas-jensen@hotmail.com>
2018-12-04 08:34:34 +0100

    Use dynamic programming in firstCycle2 and "set"-lookup in firstcycle1 Now firstCycle1 is O(n) where n is cycle length Now firstCycle2 is O(m^2) where m is input length

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,4 +1,5 @@
 import time
+
 input = open("input.txt", "r", encoding="utf-8").read()
 
 freqStrings = input.splitlines()
@@ -13,6 +14,8 @@ t4 = [+7, +7, -2, -7, -4] # first reaches 14 twice.
 t5 = [+1,-2,+3,+1] #2
 t6 = [+7, +3, +1, -13] # first reaches 11
 
+##Part 1:
+## Solution to first subquestion, that returns array for the sake of dynamic programming in part 2:
 def sumOfFrequencies(freqs):
     s = 0
     res = [0]
@@ -23,52 +26,62 @@ def sumOfFrequencies(freqs):
 
 print(sumOfFrequencies(freqs)[-1])
 
+## Part 2:
+## Solution to second subquestion that is quadratic in input length,
+## and uses dynamic programming in calculating sums of subsequences.
+## Algorithm correctness: Finding the subsequence of which the numeric sum is lowest among
+## subsequences where the sum wholly divides the sum of the entire input of frequency changes.
+## When summing the inputs (applying frequence changes), the first reoccuring value
+## will be present in the very first "round", at the index where
+##  the above mentioned sum starts or ends (depending on sign; see line 51-53).
 
-#First cycle optimized:
 def firstCycle2(freqs):
 
-    sum = sumOfFrequencies(freqs)
+    sumArr = sumOfFrequencies(freqs)
+    sum = sumArr[-1]
     candidateIndices = (0,0)
     candidateSum = 0
     for i in range(0,len(freqs)):
         for j in range(i+1,len(freqs)):
-            s = sumOfFrequencies(freqs[i:j])
+            s = sumArr[j]-sumArr[i]
             if sum==0 or s%sum == 0:
                 if candidateSum == 0 or abs(s)<abs(candidateSum):
                     candidateIndices = (i,j-1)
                     candidateSum = s
     if sum*candidateSum > 0:
-        return sumOfFrequencies(freqs[:candidateIndices[1]+1])
-    return  sumOfFrequencies(freqs[:candidateIndices[0]+1])
+        return sumArr[candidateIndices[1]+1]
+    return sumArr[candidateIndices[0]+1]
+
 
 #Printing solution
 print("exercise solution:")
-
 start = time.time()
 print(firstCycle2(freqs))
 print("{} seconds".format(time.time() - start))
 
-#Tests
-print("t1 (should be 0) : " + str(firstCycle2(t1)))
-print("t2 (should be 10) : " + str(firstCycle2(t2)))
-print("t3 (should be 5) : " + str(firstCycle2(t3)))
-print("t4 (should be 14) : " + str(firstCycle2(t4)))
-print("t5 (should be 2) : " + str(firstCycle2(t5)))
+## Solution to second subquestion. First simple algorithm.
+#  Algorithm is linear in cycle length, and performs better than the above on our input.
+#  Uses Set for expected O(1), i.e. constant, time insertion and search (membership check).
 
-
-#First cycle naive:
-def firstCycle(freqs, seen):
-    current = seen[-1]
+def firstCycle1(freqs, seen, current):
+    seen.add(current)
     for freq in freqs:
-        current += int(freq)
+        current += freq
         if current in seen:
-            print(seen)
             return current
-        seen.append(current)
-    return firstCycle(freqs, seen)
+        seen.add(current)
+    return firstCycle1(freqs, seen, current)
 
-#Solution using naive algorithm
+#Pringting solution using naive algorithm
 print("Using naive algorithm")
 start = time.time()
-print(firstCycle(freqs,[0]))
+print(firstCycle1(freqs,set(),0))
 print("{} seconds".format(time.time() - start))
+
+
+## Test cases:
+# print("t1 (should be 0) : " + str(firstCycle2(t1)))
+# print("t2 (should be 10) : " + str(firstCycle2(t2)))
+# print("t3 (should be 5) : " + str(firstCycle2(t3)))
+# print("t4 (should be 14) : " + str(firstCycle2(t4)))
+# print("t5 (should be 2) : " + str(firstCycle2(t5)))
