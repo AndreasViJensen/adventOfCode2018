4f7a61071140c1fa989e7e8cb119b6255c30120c
andreasvijensen <andreas-jensen@hotmail.com>
2018-12-04 16:55:59 +0100

    Solve second subquestion

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -1,4 +1,3 @@
-import time
 import re
 
 # Method: Filling a dict of minutes slept using stateful runthrough:
@@ -65,33 +64,73 @@ def findMostSleepyMinute(guardID, guardEvents):
         if current > max:
             max = current
             maxIndex = i
-    return maxIndex
+    return (maxIndex, max)
 
-
-
-
-
-
-#Solution:
+#Part 1: Solution:
+print("part 1:")
 input = open("input.txt", "r", encoding="utf-8").read()
 unsortedEvents = input.splitlines()
 events = sorted(unsortedEvents)
 maxGuard = findGuardWithMostSleep(events)
 print("max guard is #" + str(maxGuard))
 guardEvents = extract_guard_events(maxGuard, events)
-mostSleepyMinute = findMostSleepyMinute(maxGuard,guardEvents)
-print("Most sleepy minute is " + str(mostSleepyMinute))
+mostSleepyMinute = findMostSleepyMinute(maxGuard,guardEvents)[0]
+print("his most sleepy minute is " + str(mostSleepyMinute))
 print("answer is " + str(maxGuard*mostSleepyMinute))
 
 
-'''
-syms = ["[", "]", "-",":"]
-sortableInput = input
-for sym in syms:
-    sortableInput = sortableInput.replace(sym, "")
 
-print(sorted(sortableInput.splitlines()))
-'''
+##PART 2:
+
+# Method: Assign each event to corresponding guard in a dictionnary. Uses stateful runthrough.
+def makeGuardEventDict(events):
+    guardEventDict = dict()
+    currentGuard = 0
+    currentEventList = []
+    guardID_re = re.compile("#[0-9]+")
+    for event in events:
+        match = guardID_re.search(event)
+        if match:
+            if currentGuard in guardEventDict:
+                guardEventDict[currentGuard].extend(currentEventList)
+            else:
+                guardEventDict.update({currentGuard:currentEventList})
+            currentEventList = []
+            currentGuard = int(match.group()[1:])
+        else:
+            currentEventList.append(event)
+
+    if currentGuard in guardEventDict:
+        guardEventDict[currentGuard].extend(currentEventList)
+    else:
+        guardEventDict.update({currentGuard: currentEventList})
+
+    return guardEventDict
+
+def findGuardByStrategy2(events):
+    maxGuard = 0
+    maxFreq = 0
+    maxMinute = 0
+
+    guardEventDict = makeGuardEventDict(events)
+
+    for guard in guardEventDict:
+        res = findMostSleepyMinute(guard,guardEventDict[guard])
+        if res[1]>maxFreq:
+            maxFreq = res[1]
+            maxGuard = guard
+            maxMinute = res[0]
+    return (maxGuard,maxMinute)
 
 
+#Part 2 Solution:
+print("Part 2:")
+input = open("input.txt", "r", encoding="utf-8").read()
+unsortedEvents = input.splitlines()
+events = sorted(unsortedEvents)
 
+(resGuard,resMinute) = findGuardByStrategy2(events)
+
+print("max guard is #" + str(resGuard))
+print("His most sleepy minute is " + str(resMinute))
+print("answer is " + str(resGuard*resMinute))
